19226db782c85171813d640ef09c8d34a0ead59e
MyronBatiuk <myron.batiuk@gmail.com>
2017-08-01 13:10:57 +0300

    add default page styles

--- src/components/page/defaultPage/DefaultPage.js
+++ src/components/page/defaultPage/DefaultPage.js
@@ -1,14 +1,15 @@
 import React, {Component} from 'react';
+import './defaultPage.css';
+
 
 export default class DefaultPage extends Component {
-
 
   render() {
     const page = this.props.page;
     return (
-        <div className="page-width">
-          <h1 className="page__title">{page.title}</h1>
-          <div className="page__content" dangerouslySetInnerHTML={{__html: page.content}}></div>
+        <div className="default-page page-width">
+          <h1 className="default-page__title">{page.title}</h1>
+          <div className="default-page__content" dangerouslySetInnerHTML={{__html: page.content}}></div>
         </div>
     );
   }
